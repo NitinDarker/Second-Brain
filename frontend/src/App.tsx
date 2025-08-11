bf19f590333af01dbf6f3bf5d00d029438067209
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-08-11 17:56:53 +0530

    Tinkering with shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css'
-import Button from './components/Button'
-import { ShareIcon, PlusIcon } from './assets'
+import { Button } from './components/ui/button'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 
-function App() {
+function App () {
   return (
-    <div>
-      <Button text='Type here' variant='primary' startIcon={ShareIcon}></Button>
-      <Button text='Type here' variant='secondary' startIcon={PlusIcon}></Button>
+    <div className=''>
+      <Button
+        variant={'outline'}
+        className='bg-violet-700 text-white antialiased rounded-md hover:bg-violet-500 hover:text-white hover:cursor-pointer hover:scale-105'
+      >
+        Hey there
+      </Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
